4152c9fec02426fb43ac329b31ea69943e84a83a
mathew1357 <58839700+mathew1357@users.noreply.github.com>
2020-10-11 18:35:52 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 PASS="sevescripts"
 read -s -p "Password: " mypassword
 echo ""
-[ "$mypassword" == "$PASS" ] && echo "Password accepted" || echo "Access denied"
+[ "$mypassword" == "$PASS" ] && echo "Password accepted" || echo "Access denied" && exit
+
+echo "WELCOME TO SEVE SCRIPTS"
